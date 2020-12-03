d3b383c73b800f2edc9e7c073f38af3fe65f5140
Felippe Matheo <69490109+matheofelippe@users.noreply.github.com>
2020-12-02 22:38:58 -0300

    Códigos

---
+++ Hospedes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_individual_Tema_Vetores_Hotel
+{
+    class Hospedes
+    {
+        public string Nome { get; set; } //Get e set dos nomes dos hóspedes
+        public string Email { get; set; } //Get e set dos emails dos hóspedes
+        public Hospedes(string nome, string email) //Construtor personalizado
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString() //Metodo que retorna nome e email ao program
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio_individual_Tema_Vetores_Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hospedes[] array = new Hospedes[10]; //Cria um array com 10 espaços(0 a 9)
+            Console.Write("Quantos quartos estão alugados? "); // Pede ao usuario quantos quartos vão ser alugados
+            int alugados = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= alugados; i++)
+            {
+                Console.WriteLine("Hospede #" + i + ";");
+                Console.Write("Nome: "); // Nome do hospede
+                string nome = Console.ReadLine();
+                Console.Write("Email: "); // Email do hospede
+                string email = Console.ReadLine();
+                Console.Write("Quarto: "); // Quarto que o hospede vai alugar
+                int quarto = int.Parse(Console.ReadLine());
+                array[quarto] = new Hospedes(nome, email); // Cria um objeto hospede com nome, email e quarto alugado
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++) // Passa por todo o array
+            {
+                if (array[i] != null) //Se o espaço do array for diferente de nulo, imprime no console os dados do hospeque e o quarto alugado
+                {
+                    Console.WriteLine(i + ": " + array[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
